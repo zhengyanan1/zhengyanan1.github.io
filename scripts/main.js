c6558f7e457e932fd2c454d75760c9149ff46ce1
zhengyanan <zhengyanan.harden@bytedance.com>
2023-02-28 21:22:45 +0800

    init homepage

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/coin.png') {
+    myImage.setAttribute('src', 'images/coin2.png');
+  } else {
+    myImage.setAttribute('src', 'images/coin.png');
+  }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName){
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')){
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function(){
+  setUserName();
+}
